3c2513dc170c7d1ca1913a273ccb412b5881893d
Jeffry Degrande <jeffry@jeffrydegrande.com>
2015-09-21 03:28:29 -0300

    run shell script with reponame and require token to be passed

--- main.go
+++ main.go
@@ -1,62 +1,85 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha1"
-	"encoding/hex"
-	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
+        "flag"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "net/http"
+        "os"
+        "os/exec"
+        "encoding/json"
 )
 
 const (
-	Version = "0.1"
+        Version = "0.1"
 )
 
 var (
-	port  int
-	token string
+        port  int
+        token string
 )
+
+type Event struct {
+   PushData PushData `json:"push_data"`
+   Repository Repository `json:"repository"`
+}
+
+type PushData struct {
+  Pusher string `json:"pusher"`
+}
+
+type Repository struct {
+   RepoName string `json:"repo_name"`
+}
+
 
 var flags = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 
 func init() {
-	flags.IntVar(&port, "p", 8888, "port to run on.")
-	flags.StringVar(&token, "t", "changeme", "token that the github webhook should pass")
-
-	flags.Usage = func() {
-		fmt.Fprintf(os.Stderr, "buildbot version %s\n\n", Version)
-		fmt.Fprintf(os.Stderr, "Usage: %s [options] path\n\n", os.Args[0])
-		flags.PrintDefaults()
-	}
+        flags.IntVar(&port, "p", 8888, "port to run on.")
+        flags.StringVar(&token, "t", "changeme", "token provided through webhook url 'token' parameter")
+        flags.Usage = func() {
+                fmt.Fprintf(os.Stderr, "buildbot version %s\n\n", Version)
+                fmt.Fprintf(os.Stderr, "Usage: %s [options] path\n\n", os.Args[0])
+                flags.PrintDefaults()
+        }
 }
 
 func handleDockerHubWebhook(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
+        defer r.Body.Close()
 
-	if r.Method != "POST" {
-		http.Error(w, "405 Method not allow", http.StatusMethodNotAllowed)
-	}
+        if r.Method != "POST" {
+                http.Error(w, "405 Method not allow", http.StatusMethodNotAllowed)
+        }
 
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+        if (r.URL.Query().Get("token") != token) {
+                http.Error(w, "403 Forbidden", http.StatusForbidden)
+        }
 
-        fmt.Println(body)
+        body, err := ioutil.ReadAll(r.Body)
+        if err != nil {
+                log.Println(err.Error())
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+        }
+
+        var event Event
+        json.Unmarshal(body, &event)
+
+        fmt.Println("Pulling", event.Repository.RepoName)
+        cmd := exec.Command("sh", "build.sh", event.Repository.RepoName)
+        stdout, err := cmd.Output()
+        if err != nil {
+                fmt.Println(err.Error())
+        }
+        fmt.Println(string(stdout))
 }
 
 func main() {
-	flags.Parse(os.Args[1:])
-	log.Println("Running on port", port)
-	log.Println("Using token", token)
+        flags.Parse(os.Args[1:])
+        log.Println("Running on port", port)
 
-	http.HandleFunc("/", handleDockerHubWebhook)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+        http.HandleFunc("/", handleDockerHubWebhook)
+        log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
